b40e2dda8e88a845b42e501678e6946f4f274d29
Itskiprotich <keeprawteachjapheth@gmail.com>
2026-02-10 17:44:53 +0300

    UPD: add sql file for database operations

--- sql.sql
+++ sql.sql
@@ -1,1 +1,2 @@
 ALTER TABLE `applications` ADD `unsubmitted` TINYINT(2) NULL DEFAULT '0' AFTER `date_submitted`, ADD `initial_date_submitted` DATETIME NULL AFTER `unsubmitted`;
+ALTER TABLE `investigator_contacts` ADD `investigator_role` VARCHAR(50) NULL DEFAULT 'principal' AFTER `email`;
