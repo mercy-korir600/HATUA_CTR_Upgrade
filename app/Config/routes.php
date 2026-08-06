74132e15d7742d3328727dad06310f3dfcc4aaa8
Mercy <mkorir600@gmail.com>
2026-08-06 10:58:02 +0300

    resolve conflicts

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Routes configuration
  *
@@ -25,43 +26,41 @@
  * its action called 'display', and we pass a param to select the view file
  * to use (in this case, /app/View/Pages/home.ctp)...
  */
-	Router::parseExtensions('json', 'pdf', 'xml', 'xlsx', 'csv');
-	Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
+Router::parseExtensions('json', 'pdf', 'xml', 'xlsx', 'csv');
+Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
 /**
  * ...and connect the rest of 'Pages' controller's urls.
  */
-	Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
-	Router::connect('/api/sae/submit', array('controller' => 'saes', 'action' => 'submit'));
-
-	//APPLICANT ROUTING: DASHBOARD PAGE
-	Router::connect('/applicant', array('controller' => 'users', 'action' => 'dashboard', 'applicant' => true));	
-	//REVIEWER ROUTING: DASHBOARD PAGE
-	Router::connect('/reviewer', array('controller' => 'users', 'action' => 'dashboard', 'reviewer' => true));	
-	//PARTNER ROUTING: DASHBOARD PAGE
-	Router::connect('/partner', array('controller' => 'users', 'action' => 'dashboard', 'partner' => true));	
-	//MANAGER ROUTING: DASHBOARD PAGE
-	Router::connect('/manager', array('controller' => 'users', 'action' => 'dashboard', 'manager' => true));		
-	//ADMIN ROUTING: DASHBOARD PAGE
-	Router::connect('/admin', array('controller' => 'users', 'action' => 'dashboard', 'admin' => true));	
-	//INSPECTOR ROUTING: DASHBOARD PAGE
-	Router::connect('/inspector', array('controller' => 'users', 'action' => 'dashboard', 'inspector' => true));	
-	//MONITOR ROUTING: DASHBOARD PAGE
-	Router::connect('/monitor', array('controller' => 'users', 'action' => 'dashboard', 'monitor' => true));	 
-	//OUTSOURCE USER ROUTING: DASHBOARD PAGE
-	Router::connect('/outsource', array('controller' => 'users', 'action' => 'dashboard', 'outsource' => true));	
-	//PPB INTERNAL USER ROUTING: DASHBOARD PAGE
-	Router::connect('/internalreviewer', array('controller' => 'users', 'action' => 'dashboard', 'internalreviewer' => true));	
-	//AUDITOR USER ROUTING: DASHBOARD PAGE
-	Router::connect('/auditor', array('controller' => 'users', 'action' => 'dashboard', 'auditor' => true));
- 
+Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
+Router::connect('/api/:controller/:action/*', array('api' => true));
+//APPLICANT ROUTING: DASHBOARD PAGE
+Router::connect('/applicant', array('controller' => 'users', 'action' => 'dashboard', 'applicant' => true));
+//REVIEWER ROUTING: DASHBOARD PAGE
+Router::connect('/reviewer', array('controller' => 'users', 'action' => 'dashboard', 'reviewer' => true));
+//PARTNER ROUTING: DASHBOARD PAGE
+Router::connect('/partner', array('controller' => 'users', 'action' => 'dashboard', 'partner' => true));
+//MANAGER ROUTING: DASHBOARD PAGE
+Router::connect('/manager', array('controller' => 'users', 'action' => 'dashboard', 'manager' => true));
+//ADMIN ROUTING: DASHBOARD PAGE
+Router::connect('/admin', array('controller' => 'users', 'action' => 'dashboard', 'admin' => true));
+//INSPECTOR ROUTING: DASHBOARD PAGE
+Router::connect('/inspector', array('controller' => 'users', 'action' => 'dashboard', 'inspector' => true));
+//MONITOR ROUTING: DASHBOARD PAGE
+Router::connect('/monitor', array('controller' => 'users', 'action' => 'dashboard', 'monitor' => true));
+//OUTSOURCE USER ROUTING: DASHBOARD PAGE
+Router::connect('/outsource', array('controller' => 'users', 'action' => 'dashboard', 'outsource' => true));
+//PPB INTERNAL USER ROUTING: DASHBOARD PAGE
+Router::connect('/internalreviewer', array('controller' => 'users', 'action' => 'dashboard', 'internalreviewer' => true));
+//AUDITOR USER ROUTING: DASHBOARD PAGE
+Router::connect('/auditor', array('controller' => 'users', 'action' => 'dashboard', 'auditor' => true));
 /**
  * Load all plugin routes.  See the CakePlugin documentation on 
  * how to customize the loading of plugin routes.
  */
-	CakePlugin::routes();
+CakePlugin::routes();
 
 /**
  * Load the CakePHP default routes. Remove this if you do not want to use
  * the built-in default routes.
  */
-	require CAKE . 'Config' . DS . 'routes.php';
+require CAKE . 'Config' . DS . 'routes.php';
